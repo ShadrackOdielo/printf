85c81501d2f5439733c0915c4f0eb3294e3339f5
ShadrackOdielo <shadrackodielo@gmail.com>
2023-01-28 08:00:22 +0300

    update

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ int _printf_string(va_list args);
 int _printf_percent(void);
 int _printf_char(va_list args);
 size_t _strlen(const char *s);
+int _print_num(int n);
 
 /**
 * _printf - function that produces output according to a format
@@ -38,6 +39,10 @@ int _printf(const char *format, ...)
 		case '%':
 		chars_written += _printf_percent();
 			break;
+		case 'd':
+		case 'i':
+			chars_written += _print_num(va_arg(args, int));
+		break;
 		default:
 			chars_written += write(1, format, 1);
 		}
@@ -107,4 +112,46 @@ int _printf_char(va_list args)
 	char c = va_arg(args, int);
 
 	return (write(1, &c, 1));
+}
+
+/**
+ * _print_num - function that prints an integer
+ * @n: integer to print
+ *
+ * Return: number of characters printed
+ */
+int _print_num(int n)
+{
+	int len = 0;
+
+	if (n < 0)
+	{
+	len += write(1, "-", 1);
+	n = -n;
+	}
+
+	if (n >= 10)
+	{
+	len += _print_num(n / 10);
+	}
+
+	len += write(1, &"0123456789"[n % 10], 1);
+
+	return (len);
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int len;
+	int len2;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	return (0);
 }
